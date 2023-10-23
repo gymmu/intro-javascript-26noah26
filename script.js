58ccd09080ea288325e734f829417567ad535829
26noah26 <e262133@sbl.ch>
2023-10-23 12:12:08 +0200

    Ende Lektion 23.10

--- script.js
+++ script.js
@@ -11,3 +11,37 @@ function deleteText() {
   const elemToDeleteText = document.querySelector("#to-delete")
   elemToDeleteText.textContent = "Inhalt gelöscht"
 }
+
+function incrementCounter() {
+  const numberElement = document.querySelector("#counter")
+  let num = parseInt(numberElement.textContent)
+  num = num + 1
+  numberElement.textContent = num
+}
+
+function decrementCounter() {
+  const numberElement = document.querySelector("#counter")
+  let num = parseInt(numberElement.textContent)
+  num = num - 1
+  numberElement.textContent = num
+}
+
+function resetCounter() {
+  const numberElement = document.querySelector("#counter")
+  let num = parseInt(numberElement.textContent)
+  num = num = 0
+  numberElement.textContent = num
+}
+
+function writeText() {
+  const inputElem = document.querySelector("#input")
+  const outputElem = document.querySelector("#output")
+
+  const inputValue = inputElem.value
+  const outputValue = outputElem.textContent
+
+  const newValue = outputValue + "\n" + inputValue
+
+
+  outputElem.textContext = newValue
+}
